7897f5319bcdb8c16701ab998c6fd774b3b2eb2a
David Katende <katendedavidjoga@gmail.com>
2023-11-06 16:52:09 +0300

    Add portfolio component

---
+++ src/components/Home/index.js
@@ -0,0 +1,79 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = [' ', 'D', 'a', 'v', 'i', 'd']
+  const jobArray = [
+    'Full',
+    ' ',
+    's',
+    't',
+    'a',
+    'c',
+    'k',
+    '',
+    'W',
+    'e',
+    'b',
+    ' ',
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i, </span>
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>
+            React, Ruby on Rails, Laravel,Vue.js, Rails, JavaScript, Git, and MySQL
+          </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
